1a821873671b19239d416e1b65c87d4d27c5c497
Rob Huebner <huebnerob@gmail.com>
2016-03-18 14:32:20 -0400

    example project

---
+++ VimeoNetworking/VimeoNetworking.swift
@@ -0,0 +1,11 @@
+//
+//  VimeoNetworking.swift
+//  VimeoNetworkingExample-iOS
+//
+//  Created by Huebner, Rob on 3/17/16.
+//  Copyright © 2016 Vimeo. All rights reserved.
+//
+
+import Foundation
+
+class VimeoNetworking { }
---
+++ VimeoNetworkingExample-iOS/VimeoNetworkingExample-iOS/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  VimeoNetworkingExample-iOS
+//
+//  Created by Huebner, Rob on 3/17/16.
+//  Copyright © 2016 Vimeo. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    var detailItem: AnyObject? {
+        didSet {
+            // Update the view.
+            self.configureView()
+        }
+    }
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = self.detailItem {
+            if let label = self.detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.configureView()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
