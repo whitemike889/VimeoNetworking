7ca34b396a1ac64a668fade18937957340d28b6e
Rob Huebner <huebnerob@gmail.com>
2016-04-05 16:34:37 -0400

    tweaking

--- VimeoNetworking/Response.swift
+++ VimeoNetworking/Response.swift
@@ -12,5 +12,9 @@ struct Response<ModelType: MappableResponse>
 {
     let model: ModelType
     
-    let nextPagePath: String? = nil
+    let nextPagePath: String? = nil // TODO:  [RH] (4/5/16)
+    var nextPageRequest: Request<ModelType>?
+    {
+        return nil // TODO: computed from nextPagePath [RH] (4/5/16)
+    }
 }
--- VimeoNetworkingExample-iOS/VimeoNetworkingExample-iOS/AppDelegate.swift
+++ VimeoNetworkingExample-iOS/VimeoNetworkingExample-iOS/AppDelegate.swift
@@ -114,7 +114,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
     
     func application(app: UIApplication, openURL url: NSURL, options: [String : AnyObject]) -> Bool
     {
-        self.authenticationController?.codeGrant(responseURL: url, completion: { (result) in
+        self.authenticationController?.codeGrant(responseURL: url, completion: { result in
             
             switch result
             {
@@ -131,7 +131,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
 
     // MARK: - Split view
 
-    func splitViewController(splitViewController: UISplitViewController, collapseSecondaryViewController secondaryViewController:UIViewController, ontoPrimaryViewController primaryViewController:UIViewController) -> Bool
+    func splitViewController(splitViewController: UISplitViewController, collapseSecondaryViewController secondaryViewController: UIViewController, ontoPrimaryViewController primaryViewController: UIViewController) -> Bool
     {
         guard let secondaryAsNavController = secondaryViewController as? UINavigationController else { return false }
         guard let topAsDetailController = secondaryAsNavController.topViewController as? DetailViewController else { return false }
