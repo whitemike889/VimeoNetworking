f81257c19e985a2eb668866f66cbcc1f8bb7dc50
Rob Huebner <huebnerob@gmail.com>
2016-04-27 11:38:04 -0400

    first pass at KeychainStore

--- VimeoNetworking/VimeoNetworking/KeychainStore.swift
+++ VimeoNetworking/VimeoNetworking/KeychainStore.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import Security
 
 internal protocol SecureDataStore
 {
@@ -19,28 +20,114 @@ internal protocol SecureDataStore
 
 final internal class KeychainStore: SecureDataStore
 {
-    // TODO:  [RH] (3/24/16)
+    let service: String
+    let accessGroup: String?
     
-    init(service: String, accessGroup: String)
+    init(service: String, accessGroup: String?)
     {
-        // TODO:  [RH] (3/24/16)
+        self.service = service
+        self.accessGroup = accessGroup
     }
     
     func setData(data: NSData, forKey key: String) throws
     {
-        // TODO:  [RH] (3/24/16)
+        try self.deleteDataForKey(key)
+        
+        var query = self.queryForKey(key)
+        
+        query[kSecValueData as String] = data
+        query[kSecAttrAccessible as String] = kSecAttrAccessibleAfterFirstUnlock as String
+        
+        let status = SecItemAdd(query, nil)
+        
+        if status != errSecSuccess
+        {
+            throw self.errorForStatus(status)
+        }
     }
     
     func dataForKey(key: String) throws -> NSData?
     {
-        // TODO:  [RH] (3/24/16)
+        var query = self.queryForKey(key)
         
-        return nil
+        query[kSecMatchLimit as String] = kSecMatchLimitOne as String
+        query[kSecReturnData as String] = kCFBooleanTrue
+        
+        var attributes: AnyObject? = nil
+        let status = SecItemCopyMatching(query, &attributes)
+        let data = attributes as? NSData
+        
+        if status != errSecSuccess
+        {
+            throw self.errorForStatus(status)
+        }
+        
+        return data
     }
     
     func deleteDataForKey(key: String) throws
     {
-        // TODO:  [RH] (3/24/16)
+        let query = self.queryForKey(key)
+        
+        let status = SecItemDelete(query)
+        
+        if status != errSecSuccess
+        {
+            throw self.errorForStatus(status)
+        }
+    }
+    
+    // MARK: - 
+    
+    private func queryForKey(key: String) -> [String: AnyObject]
+    {
+        var query: [String: AnyObject] = [:]
+        
+        query[kSecClass as String] = kSecClassGenericPassword as String
+        query[kSecAttrService as String] = self.service
+        query[kSecAttrAccount as String] = key
+        
+        if let accessGroup = self.accessGroup
+        {
+            query[kSecAttrAccessGroup as String] = accessGroup
+        }
+        
+        return query
+    }
+    
+    private func errorForStatus(status: OSStatus) -> NSError
+    {
+        let errorMessage: String
+        
+        switch status
+        {
+        case errSecSuccess:
+            errorMessage = "success"
+        case errSecUnimplemented:
+            errorMessage = "Function or operation not implemented."
+        case errSecParam:
+            errorMessage = "One or more parameters passed to the function were not valid."
+        case errSecAllocate:
+            errorMessage = "Failed to allocate memory."
+        case errSecNotAvailable:
+            errorMessage = "No trust results are available."
+        case errSecAuthFailed:
+            errorMessage = "Authorization/Authentication failed."
+        case errSecDuplicateItem:
+            errorMessage = "The item already exists."
+        case errSecItemNotFound:
+            errorMessage = "The item cannot be found."
+        case errSecInteractionNotAllowed:
+            errorMessage = "Interaction with the Security Server is not allowed."
+        case errSecDecode:
+            errorMessage = "Unable to decode the provided data."
+        default:
+            errorMessage = "undefined error"
+        }
+        
+        let error = NSError(domain: "KeychainErrorDomain", code: Int(status), userInfo: [NSLocalizedDescriptionKey: errorMessage])
+        
+        return error
     }
 }
 
