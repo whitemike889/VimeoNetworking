ffb073013f842518296644f9ab8a5bcfed8f7c41
addisonbean <addisonbean@gmail.com>
2016-09-03 14:16:29 -0500

    Add secure link for VIMVideoFile

--- VimeoNetworking/VimeoNetworking/Models/VIMVideoFile.h
+++ VimeoNetworking/VimeoNetworking/Models/VIMVideoFile.h
@@ -40,6 +40,7 @@ extern NSString *const __nonnull VIMVideoFileQualityMobile;
 @property (nonatomic, strong, nullable) NSNumber *height;
 @property (nonatomic, strong, nullable) NSNumber *size;
 @property (nonatomic, copy, nullable) NSString *link;
+@property (nonatomic, copy, nullable) NSString *linkSecure;
 @property (nonatomic, copy, nullable) NSString *quality;
 @property (nonatomic, copy, nullable) NSString *type;
 @property (nonatomic, strong, nullable) VIMVideoLog *log;
