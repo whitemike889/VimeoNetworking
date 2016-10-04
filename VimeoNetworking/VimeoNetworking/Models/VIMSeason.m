60c6e4c39cc7c61d620bf264831f50e9fb3e535e
Huebner, Rob <robh@vimeo.com>
2016-10-04 17:02:13 -0400

    adding vimseason

---
+++ VimeoNetworking/VimeoNetworking/Models/VIMSeason.m
@@ -0,0 +1,74 @@
+//
+//  VIMSeason.m
+//  Pods
+//
+//  Created by Huebner, Rob on 10/4/16.
+//
+//
+
+#import "VIMSeason.h"
+
+@interface VIMSeason ()
+
+@property (nonatomic, strong) NSDictionary *metadata;
+@property (nonatomic, strong) NSDictionary *connections;
+@property (nonatomic, strong) NSDictionary *interactions;
+
+@end
+
+@implementation VIMSeason
+
+- (void)didFinishMapping
+{
+    [self parseConnections];
+    [self parseInteractions];
+}
+
+#pragma mark - Parsing Helpers
+
+- (void)parseConnections
+{
+    NSMutableDictionary *connections = [NSMutableDictionary dictionary];
+    
+    NSDictionary *dict = [self.metadata valueForKey:@"connections"];
+    if([dict isKindOfClass:[NSDictionary class]])
+    {
+        for(NSString *key in [dict allKeys])
+        {
+            NSDictionary *value = [dict valueForKey:key];
+            if([value isKindOfClass:[NSDictionary class]])
+            {
+                VIMConnection *connection = [[VIMConnection alloc] initWithKeyValueDictionary:value];
+                [connections setObject:connection forKey:key];
+            }
+        }
+    }
+    
+    self.connections = connections;
+}
+
+- (void)parseInteractions
+{
+    NSMutableDictionary *interactions = [NSMutableDictionary dictionary];
+    
+    NSDictionary *dict = [self.metadata valueForKey:@"interactions"];
+    if([dict isKindOfClass:[NSDictionary class]])
+    {
+        for(NSString *key in [dict allKeys])
+        {
+            NSDictionary *value = [dict valueForKey:key];
+            if([value isKindOfClass:[NSDictionary class]])
+            {
+                VIMInteraction *interaction = [[VIMInteraction alloc] initWithKeyValueDictionary:value];
+                if([interaction respondsToSelector:@selector(didFinishMapping)])
+                    [interaction didFinishMapping];
+                
+                [interactions setObject:interaction forKey:key];
+            }
+        }
+    }
+    
+    self.interactions = interactions;
+}
+
+@end
