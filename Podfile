e4b0fea8673a31af06f5144db213c62f3963a908
jasonhawkins <hellojason@me.com>
2016-12-14 18:05:41 -0500

    Add tvOS framework target and shared scheme.

--- Podfile
+++ Podfile
@@ -3,11 +3,19 @@ use_frameworks!
 workspace 'VimeoNetworking'
 project 'VimeoNetworkingExample-iOS/VimeoNetworkingExample-iOS.xcodeproj'
 
-target 'VimeoNetworkingExample-iOS' do
-    pod 'AFNetworking', '3.1.0'
+def shared_pods
+	pod 'AFNetworking', '3.1.0'
     pod 'VimeoNetworking', :path => '../VimeoNetworking'
+end
+
+target 'VimeoNetworkingExample-iOS' do
+    shared_pods
 
     target 'VimeoNetworkingExample-iOSTests' do
     	inherit! :search_paths
 	end
+end
+
+target 'VimeoNetworkingtvOS' do
+	shared_pods
 end
---
+++ VimeoNetworkingExample-iOS/VimeoNetworking tvOS/VimeoNetworking tvOS.h
@@ -0,0 +1,19 @@
+//
+//  VimeoNetworking tvOS.h
+//  VimeoNetworking tvOS
+//
+//  Created by Hawkins, Jason on 12/14/16.
+//  Copyright © 2016 Vimeo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for VimeoNetworking tvOS.
+FOUNDATION_EXPORT double VimeoNetworking_tvOSVersionNumber;
+
+//! Project version string for VimeoNetworking tvOS.
+FOUNDATION_EXPORT const unsigned char VimeoNetworking_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VimeoNetworking_tvOS/PublicHeader.h>
+
+
