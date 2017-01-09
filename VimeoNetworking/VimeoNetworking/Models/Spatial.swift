ec2ff3258fd8a13038d47bb1a534784c55a034f3
Lim, Jennifer <jennifer@vimeo.com>
2017-01-09 10:44:42 -0500

    Add Spatial object to handle three sixty video.

---
+++ VimeoNetworking/VimeoNetworking/Models/Spatial.swift
@@ -0,0 +1,39 @@
+//
+//  Spatial.swift
+//  Pods
+//
+//  Created by Lim, Jennifer on 1/6/17.
+//
+//
+//
+
+public class Spatial: VIMModelObject
+{
+    enum Projection: String
+    {
+        case Equirectangular = "equirectangular"
+        case Cylindrical = "cylindrical"
+        case Cubical = "cubical"
+        case Pyramid = "pyramid"
+        case Dome = "dome"
+    }
+    
+    enum Format: String
+    {
+        case Mono = "mono"
+        case LeftRight = "left-right"
+        case TopBottom = "top-bottom"
+    }
+    
+    // MARK: - Properties
+    
+    public dynamic var projection: String?
+    public dynamic var stereoFormat: String?
+    
+    // MARK: - VIMMappable
+    
+    public override func getObjectMapping() -> AnyObject!
+    {
+        return ["stereo_format" : "stereoFormat"]
+    }
+}
