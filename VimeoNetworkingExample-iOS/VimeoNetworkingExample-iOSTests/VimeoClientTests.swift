fb78be42e2e62be6fdd0d57585f16c3605f875e9
Fiel Guhit <fiel@vimeo.com>
2017-01-17 13:40:53 -0500

    Added notification unit tests

--- VimeoNetworkingExample-iOS/VimeoNetworkingExample-iOSTests/VimeoClientTests.swift
+++ VimeoNetworkingExample-iOS/VimeoNetworkingExample-iOSTests/VimeoClientTests.swift
@@ -7,29 +7,63 @@
 //
 
 import XCTest
+@testable import VimeoNetworkingExample_iOS
 
-class VimeoClientTests: XCTestCase {
+class VimeoClientTests: XCTestCase
+{
+    let configuration = AppConfiguration(clientIdentifier: "13175bcf4414a49c8ce74be465c441c3daec9de9",
+                                         clientSecret: "22d869d54feb4ee729b9fd87802c9cedcf3a9b71",
+                                         scopes: [.Public, .Private, .Purchased, .Create, .Edit, .Delete, .Interact, .Upload],
+                                         keychainService: "com.vimeo.keychain_service",
+                                         apiVersion: "3.3")
     
-    override func setUp() {
-        super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    lazy var client: VimeoClient =
+    {
+        return VimeoClient(appConfiguration: self.configuration)
+    }()
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testInitialCurrentAccountNotificationUserInfo()
+    {
+        let expectation = self.expectationWithDescription("Wait for initial account notification")
+        let token: ObservationToken? = Notification.AuthenticatedAccountDidChange.observe() { [unowned self] (notification: NSNotification) in
+            XCTAssert(self.client.currentAccount != nil)
+            XCTAssert(notification.userInfo != nil)
+            XCTAssert(notification.userInfo![UserInfoKey.PreviousAccount.rawValue] as? VIMAccount == nil)
+            
+            expectation.fulfill()
         }
+        
+        XCTAssert(token != nil)
+        
+        self.client.currentAccount = VIMAccount()
+        
+        self.waitForExpectationsWithTimeout(10, handler: nil)
+        
+        Notification.AuthenticatedAccountDidChange.removeObserver(self)
     }
     
+    func testSubsequentAccountNotificationUserInfo()
+    {
+        let firstAccount = VIMAccount()
+        let secondAccount = VIMAccount()
+        self.client.currentAccount = firstAccount
+        
+        let expectation = self.expectationWithDescription("Wait for subsequent account notification")
+        let token: ObservationToken? = Notification.AuthenticatedAccountDidChange.observe() { (notification: NSNotification) in
+            XCTAssert(notification.userInfo != nil)
+            
+            if let previousAccount = notification.userInfo![UserInfoKey.PreviousAccount.rawValue] as? VIMAccount {
+                XCTAssert(previousAccount == firstAccount)
+                expectation.fulfill()
+            }
+        }
+        
+        XCTAssert(token != nil)
+        
+        self.client.currentAccount = secondAccount
+        
+        self.waitForExpectationsWithTimeout(10, handler: nil)
+        
+        Notification.AuthenticatedAccountDidChange.removeObserver(self)
+    }
 }
