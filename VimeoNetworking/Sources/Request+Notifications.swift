9d0ee5b0f2490c6ce3350e6988df0426d77531c0
Fiel Guhit <fiel@vimeo.com>
2017-02-13 17:30:04 -0500

    Add request extension for notification methods

--- VimeoNetworking/Sources/Request+Notifications.swift
+++ VimeoNetworking/Sources/Request+Notifications.swift
@@ -8,7 +8,7 @@
 
 public extension Request
 {
-    private static var Path: String { return "/me/notifications/subscriptions" }
+    private static var Path: String { return "/me/notifications" }
     
     /// Create a request that updates the push notification subscriptions
     ///
@@ -23,6 +23,36 @@ public extension Request
                         "reply": subscription.reply,
                         "follow": subscription.follow,
                         "video_available": subscription.videoAvailable]
+        
+        return Request(method: .PATCH, path: Path.stringByAppendingString("/subscriptions"), parameters: parameters)
+    }
+    
+    public static func markLatestNotification(notification: VIMNotification) -> Request
+    {
+        var parameters:[String: AnyObject]? = nil
+        if let uri = notification.uri
+        {
+            parameters = [
+                "latest_notification_uri" : uri,
+                "new" : true
+            ]
+        }
+        
+        return Request(method: .PATCH, path: Path, parameters: parameters)
+    }
+    
+    public static func seenNotifications(notifications: [VIMNotification]) -> Request
+    {
+        var parameters: [[String: AnyObject]] = []
+        notifications.map { (notification: VIMNotification) -> Void in
+            if let uri = notification.uri
+            {
+                parameters.append([
+                    "seen": true,
+                    "uri": uri]
+                )
+            }
+        }
         
         return Request(method: .PATCH, path: Path, parameters: parameters)
     }
