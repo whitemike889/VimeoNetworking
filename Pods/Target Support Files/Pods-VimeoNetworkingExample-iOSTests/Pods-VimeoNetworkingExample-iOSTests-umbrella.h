7538f41cee81ee38e2b31d16f527c8a2a22c9f15
wilrnh <wilrnh@msn.com>
2017-02-28 10:50:05 -0500

    Add pods to git

---
+++ Pods/Target Support Files/Pods-VimeoNetworkingExample-iOSTests/Pods-VimeoNetworkingExample-iOSTests-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_VimeoNetworkingExample_iOSTestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_VimeoNetworkingExample_iOSTestsVersionString[];
+
