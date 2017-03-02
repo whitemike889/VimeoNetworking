1b0ee9b67f3cabd8d755d7b205c1e64e81b3ec85
jasonhawkins <hellojason@me.com>
2017-03-02 17:11:17 -0500

    Add PlayProgress VIMModelObject subclass.

---
+++ VimeoNetworking/Sources/Models/PlayProgress.swift
@@ -0,0 +1,16 @@
+//
+//  Progress.swift
+//  Pods
+//
+//  Created by Hawkins, Jason on 3/2/17.
+//
+//
+
+import Foundation
+
+/// An object representing the amount of progress a video has been viewed.
+public class PlayProgress: VIMModelObject
+{
+    /// The time, in seconds, that the video has been viewed.
+    public var seconds: Int?
+}
