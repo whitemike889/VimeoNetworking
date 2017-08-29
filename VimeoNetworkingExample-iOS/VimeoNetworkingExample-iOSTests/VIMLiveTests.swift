d77675dfda4f4137ae23e0155e0be8a8a69ec8d1
Van Le Nguyen <van@vimeo.com>
2017-08-29 11:53:40 -0400

    Add unit-test files

---
+++ VimeoNetworkingExample-iOS/VimeoNetworkingExample-iOSTests/VIMLiveTests.swift
@@ -0,0 +1,17 @@
+//
+//  VIMLiveTests.swift
+//  VimeoNetworkingExample-iOS
+//
+//  Created by Nguyen, Van on 8/29/17.
+//  Copyright © 2017 Vimeo. All rights reserved.
+//
+
+import XCTest
+
+class VIMLiveTests: XCTestCase
+{
+    func testParsingLiveObject()
+    {
+        
+    }
+}
