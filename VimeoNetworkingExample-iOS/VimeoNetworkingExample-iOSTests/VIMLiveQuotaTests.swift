1f36d01928598401f4819923f19c9e215ddb3dd3
Van Le Nguyen <van@vimeo.com>
2017-09-11 17:24:10 -0400

    Add unit test file for `VIMLiveQuota`

---
+++ VimeoNetworkingExample-iOS/VimeoNetworkingExample-iOSTests/VIMLiveQuotaTests.swift
@@ -0,0 +1,14 @@
+//
+//  VIMLiveQuotaTests.swift
+//  VimeoNetworkingExample-iOS
+//
+//  Created by Nguyen, Van on 9/11/17.
+//  Copyright © 2017 Vimeo. All rights reserved.
+//
+
+import XCTest
+
+class VIMLiveQuotaTests: XCTestCase
+{
+    
+}
