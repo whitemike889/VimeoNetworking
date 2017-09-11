5c79a347db2f0bb09733119869805607621faba7
Van Le Nguyen <van@vimeo.com>
2017-09-11 17:37:00 -0400

    Refactor json-reading function into a separate file

--- VimeoNetworkingExample-iOS/VimeoNetworkingExample-iOSTests/TestingUtility.swift
+++ VimeoNetworkingExample-iOS/VimeoNetworkingExample-iOSTests/TestingUtility.swift
@@ -1,8 +1,8 @@
 //
-//  VIMLiveTests.swift
+//  TestingUtility.swift
 //  VimeoNetworkingExample-iOS
 //
-//  Created by Van Nguyen on 08/29/2017.
+//  Created by Van Nguyen on 09/11/2017.
 //  Copyright (c) Vimeo (https://vimeo.com)
 //
 //  Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -24,20 +24,39 @@
 //  THE SOFTWARE.
 //
 
-import XCTest
+import Foundation
 import VimeoNetworking
 
-class VIMLiveTests: XCTestCase
+class TestingUtility
 {
-    func testParsingLiveObject()
+    static func videoObjectFromFile(named fileName: String) -> VIMVideo
     {
-        let video = TestingUtility.videoObjectFromFile(named: "clip_live.json")
-        XCTAssertNotNil(video.live)
-        XCTAssertEqual(video.live?.link, "rtmp://rtmp.cloud.vimeo.com/live?token=b23a326b-eb96-432d-97d5-122afa3a4e47")
-        XCTAssertEqual(video.live?.key, "42f9947e-6bb6-4119-bc37-8ee9d49c8567")
-        XCTAssertEqual(video.live?.activeTime?.description, "2017-08-01T18:18:44+00:00")
-        XCTAssertNil(video.live?.endedTime)
-        XCTAssertNil(video.live?.archivedTime)
-        XCTAssertEqual(video.live?.liveStreamingStatus, .streaming)
+        do
+        {
+            guard let fileURL = Bundle(for: TestingUtility.self).url(forResource: fileName, withExtension: nil) else
+            {
+                assertionFailure("Error: Cannot locate test file!")
+                return VIMVideo()
+            }
+            
+            let data = try Data(contentsOf: fileURL)
+            let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+            
+            let mapper = VIMObjectMapper()
+            mapper.addMappingClass(VIMVideo.self, forKeypath: "")
+            
+            guard let video = mapper.applyMapping(toJSON: json) as? VIMVideo else
+            {
+                assertionFailure("Error: Cannot map JSON data to VIMVideo!")
+                return VIMVideo()
+            }
+            
+            return video
+        }
+        catch let error
+        {
+            assertionFailure("Error: \(error.localizedDescription)")
+            return VIMVideo()
+        }
     }
 }
