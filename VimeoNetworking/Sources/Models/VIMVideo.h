fe146fbf5c477b1d184601dba9280c147fadd300
jasonhawkins <hellojason@me.com>
2017-09-28 11:21:28 -0400

    Fix typos in documentation

--- VimeoNetworking/Sources/Models/VIMVideo.h
+++ VimeoNetworking/Sources/Models/VIMVideo.h
@@ -115,14 +115,14 @@ typedef NS_ENUM(NSUInteger, VIMVideoProcessingStatus) {
 /**
  Checks for the existence of a Spatial object on this VIMVideo and returns @p true if one exists.
  
- @return Returns @p true is a Spatial object exists for this VIMVideo.
+ @return Returns @p true if a Spatial object exists for this VIMVideo.
  */
 - (BOOL)is360;
 
 /**
  Checks for the existence of a VIMLive object on this VIMVideo and returns @p true if one exists, but does not check the state of the live event.
 
- @return Returns @p true is a VIMLive object exists for this VIMVideo.
+ @return Returns @p true if a VIMLive object exists for this VIMVideo.
  */
 - (BOOL)isLive;
 
