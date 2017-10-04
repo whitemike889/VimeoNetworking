b70d783678690454e0c942ef13bb65fb166c65c8
Van Le Nguyen <van@vimeo.com>
2017-10-04 10:12:40 -0400

    Add unit test file for testing `VIMLiveHeartbeat`

---
+++ VimeoNetworkingExample-iOS/VimeoNetworkingExample-iOSTests/VIMLiveHeartbeatTests.swift
@@ -0,0 +1,14 @@
+//
+//  VIMLiveHeartbeatTests.swift
+//  VimeoNetworkingExample-iOS
+//
+//  Created by Nguyen, Van on 10/4/17.
+//  Copyright © 2017 Vimeo. All rights reserved.
+//
+
+import XCTest
+
+class VIMLiveHeartbeatTests: XCTestCase
+{
+    
+}
