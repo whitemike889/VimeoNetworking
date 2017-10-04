5d8c2357389dbfb20e1b235c342250740acc7f1b
Van Le Nguyen <van@vimeo.com>
2017-10-04 10:06:36 -0400

    Make `VIMLiveHeartbeat` class public

--- VimeoNetworking/Sources/Models/VIMLiveHeartbeat.swift
+++ VimeoNetworking/Sources/Models/VIMLiveHeartbeat.swift
@@ -8,16 +8,16 @@
 
 import Foundation
 
-class VIMLiveHeartbeat: VIMModelObject
+public class VIMLiveHeartbeat: VIMModelObject
 {
     private struct Constants
     {
         static let HeartbeatUrlKey = "heartbeat"
     }
     
-    private(set) var heartbeatUrl: String?
+    public private(set) var heartbeatUrl: String?
     
-    override func getObjectMapping() -> Any?
+    override public func getObjectMapping() -> Any?
     {
         return [Constants.HeartbeatUrlKey: "heartbeatUrl"]
     }
