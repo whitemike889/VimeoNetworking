5c94ece29330264d9668e524b86786c5098b651a
Van Le Nguyen <van@vimeo.com>
2017-10-04 09:48:56 -0400

    Provide mapping from `live->heartbeat` to `heartbeatUrl`

--- VimeoNetworking/Sources/Models/VIMLiveHeartbeat.swift
+++ VimeoNetworking/Sources/Models/VIMLiveHeartbeat.swift
@@ -10,5 +10,15 @@ import Foundation
 
 class VIMLiveHeartbeat: VIMModelObject
 {
+    private struct Constants
+    {
+        static let HeartbeatUrlKey = "heartbeat"
+    }
+    
     private(set) var heartbeatUrl: String?
+    
+    override func getObjectMapping() -> Any?
+    {
+        return [Constants.HeartbeatUrlKey: "heartbeatUrl"]
+    }
 }
