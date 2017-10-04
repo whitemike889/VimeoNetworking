5f301fc2f214121e370ea353c6e0034a56919987
Van Le Nguyen <van@vimeo.com>
2017-10-04 09:45:20 -0400

    Add hearbeat url to `VIMLiveHeartbeat`

---
+++ VimeoNetworking/Sources/Models/VIMLiveHeartbeat.swift
@@ -0,0 +1,14 @@
+//
+//  VIMLiveHeartbeat.swift
+//  Pods
+//
+//  Created by Nguyen, Van on 10/4/17.
+//
+//
+
+import Foundation
+
+class VIMLiveHeartbeat: VIMModelObject
+{
+    private(set) var heartbeatUrl: String?
+}
