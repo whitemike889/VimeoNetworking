84aec90a3be9cc51a463c39c4f7f56ce8540a5fe
jasonhawkins <hellojason@me.com>
2018-03-05 19:40:49 -0500

    Remove unnecessary Objective-C constants

--- VimeoNetworkingExample-iOS/VimeoNetworkingExample-iOSTests/VIMLiveTests.swift
+++ VimeoNetworkingExample-iOS/VimeoNetworkingExample-iOSTests/VIMLiveTests.swift
@@ -69,7 +69,7 @@ class VIMLiveTests: XCTestCase
     private func assert(liveChatUserObject user: VIMLiveChatUser?)
     {
         XCTAssertNotNil(user)
-        XCTAssertEqual(user?.account, VIMLiveChatUser.AccountTypeLiveBusiness)
+        XCTAssertEqual(user?.account, AccountType.liveBusiness.rawValue)
         XCTAssertEqual(user?.id?.int64Value, MockLiveChatUser.Id)
         XCTAssertEqual(user?.name, MockLiveChatUser.Name)
         XCTAssertEqual(user?.isStaff?.boolValue, true)
