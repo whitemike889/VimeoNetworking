39120919baa83015a0dd9a2c29b95fc1fedf4afb
Jennifer Lim <jennifer@vimeo.com>
2018-04-09 10:52:14 -0400

    Add documentation

--- VimeoNetworking/Sources/Models/VIMSizeQuota.swift
+++ VimeoNetworking/Sources/Models/VIMSizeQuota.swift
@@ -7,7 +7,12 @@
 
 public class VIMSizeQuota: VIMModelObject
 {
+    /// Indicates the free space size
     @objc dynamic public private(set) var free: NSNumber?
+
+    /// Indicates the maximum size quota
     @objc dynamic public private(set) var max: NSNumber?
+
+    /// Indicates the used size quota
     @objc dynamic public private(set) var used: NSNumber?
 }
