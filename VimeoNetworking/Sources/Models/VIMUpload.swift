f72d17fccd5fca52383bedb1d15cf1180eb5019d
Nicole Lehrer <nicole@vimeo.com>
2018-04-18 16:34:12 -0400

    Added stub for new upload object.

---
+++ VimeoNetworking/Sources/Models/VIMUpload.swift
@@ -0,0 +1,19 @@
+//
+//  VIMUpload.swift
+//  Pods
+//
+//  Created by Lehrer, Nicole on 4/18/18.
+//
+
+import Foundation
+
+// TODO - fill in, this is a very incomplete list of properties on this class.
+
+public class VIMUpload: VIMModelObject
+{
+    @objc dynamic public private(set) var approach: String?
+    
+    @objc dynamic public private(set) var link: String?
+    
+    @objc dynamic public private(set) var uploadLink: String?
+}
