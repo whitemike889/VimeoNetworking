6b50792fdad7896d0d9b7be4d94434756b33a716
Van Le Nguyen <van@vimeo.com>
2019-02-22 14:16:02 -0500

    Add white space

--- VimeoNetworking/Sources/Models/VIMVideo.h
+++ VimeoNetworking/Sources/Models/VIMVideo.h
@@ -171,6 +171,7 @@ typedef NS_ENUM(NSUInteger, VIMVideoProcessingStatus) {
 - (void)setIsLiked:(BOOL)isLiked;
 - (void)setIsWatchLater:(BOOL)isWatchLater;
 - (BOOL)hasReviewPage;
+
 /**
  Determines if the video has a file transfer page. A video has a file
  transfer page if @p canDownload privacy is @p true and there is a
