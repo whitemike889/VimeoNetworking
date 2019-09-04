ea285c43680943f4d2c2ec1834883a571817bf77
Rogerio de Paula Assis <rogerio@vimeo.com>
2019-09-04 15:51:51 -0400

    [VIM-7221] Replaces AFNetworking's reachability manager (#351)
    * Adds NetworkReachabilityManager to project
    
    * Refactor VimeoReachability into VimeoReachabilityMonitor
    
    * Refactors VimeoClient to inject ReachabilityMonitoring
    
    * Changed reachabilityChanged closure to non-optional
    
    * Changed visibility of VimeoReachabilityMonitoring
    
    * Changed initializer to internal
    
    * Un-nested reachability status so it can be public typealiased
    
    * Moved reachability status enum to separate file, added unit tests
    
    * Major refactoring to abstract NetworkReachabilityManager and remove VimeoReachability
    
    * Isolated properties and tests unavailable on macOS
    
    * Renames VimeoReachabilityMonitor to VimeoReachabilityProvider
    
    * Unit test renames, code tidy up
    
    * Adds more documentation to VimeoReachabilityProvider
    
    * Removes start and stop listening methods from reachability protocol
    
    * Removes unused VimeoReachabilityMonitorTests.swift
    
    * Renames test methods
    
    * Cosmetic changes to spacing
    
    * Addresses PR feedback
    
    * Updates unit tests naming convention to follow styleguide

---
+++ Tests/Shared/Reachability/NetworkingReachabilityManagerTests.swift
@@ -0,0 +1,161 @@
+//
+//  NetworkingReachabilityManagerTests.swift
+//  VimeoNetworking
+//
+//  Created by Rogerio de Paula Assis on 8/29/19.
+//  Copyright © 2019 Vimeo. All rights reserved.
+//
+
+import XCTest
+@testable import VimeoNetworking
+
+class NetworkingReachabilityManagerTests: XCTestCase {
+
+    func test_Initializer_InstanceIsNotNull_WhenInitializedFromHost() {
+        // Given, When
+        let manager = NetworkReachabilityManager(host: "localhost")
+        
+        // Then
+        XCTAssertNotNil(manager)
+    }
+
+    func test_Initializer_InstanceIsNotNull_WhenInitializedFromAddress() {
+        // Given, When
+        let manager = NetworkReachabilityManager()
+
+        // Then
+        XCTAssertNotNil(manager)
+    }
+
+    func test_InitializedFromHost_StartsWithReachableOnWiFi() {
+        // Given, When
+        let manager = NetworkReachabilityManager(host: "localhost")
+
+        // Then
+        XCTAssertEqual(manager?.status, .reachable(.ethernetOrWiFi))
+        XCTAssertEqual(manager?.isReachable, true)
+        XCTAssertEqual(manager?.isReachableOnCellular, false)
+        XCTAssertEqual(manager?.isReachableOnEthernetOrWiFi, true)
+    }
+
+    func test_DefaultInitializer_StartsWithReachableOnWifi() {
+        // Given, When
+        let manager = NetworkReachabilityManager()
+
+        // Then
+        XCTAssertEqual(manager?.status, .reachable(.ethernetOrWiFi))
+        XCTAssertEqual(manager?.isReachable, true)
+        XCTAssertEqual(manager?.isReachableOnCellular, false)
+        XCTAssertEqual(manager?.isReachableOnEthernetOrWiFi, true)
+    }
+
+    func test_DefaultInitializedInstance_CanBeProperlyRestarted() {
+        // Given
+        let manager = NetworkReachabilityManager()
+        let first = expectation(description: "first listener notified")
+        let second = expectation(description: "second listener notified")
+
+        // When
+        manager?.startListening { (status) in
+            first.fulfill()
+        }
+        wait(for: [first], timeout: 1)
+
+        manager?.stopListening()
+
+        manager?.startListening { (status) in
+            second.fulfill()
+        }
+        wait(for: [second], timeout: 1)
+
+        // Then
+        XCTAssertEqual(manager?.status, .reachable(.ethernetOrWiFi))
+    }
+
+    func test_HostInitializedInstance_CanBeProperlyRestarted() {
+        // Given
+        let manager = NetworkReachabilityManager(host: "localhost")
+        let first = expectation(description: "first listener notified")
+        let second = expectation(description: "second listener notified")
+
+        // When
+        manager?.startListening { (status) in
+            first.fulfill()
+        }
+        wait(for: [first], timeout: 1)
+
+        manager?.stopListening()
+
+        manager?.startListening { (status) in
+            second.fulfill()
+        }
+        wait(for: [second], timeout: 1)
+
+        // Then
+        XCTAssertEqual(manager?.status, .reachable(.ethernetOrWiFi))
+    }
+
+    func testThatHostManagerCanBeDeinitialized() {
+        // Given
+        var manager = NetworkReachabilityManager(host: "localhost")
+
+        // When
+        manager = nil
+
+        // Then
+        XCTAssertNil(manager)
+    }
+
+    func testThatAddressManagerCanBeDeinitialized() {
+        // Given
+        var manager: NetworkReachabilityManager? = NetworkReachabilityManager()
+
+        // When
+        manager = nil
+
+        // Then
+        XCTAssertNil(manager)
+    }
+
+    // MARK: - Listener
+
+    func test_HostInitializer_StartListening_ClosureIsCalled_WhenStartListeningIsCalled() {
+        // Given
+        guard let manager = NetworkReachabilityManager(host: "localhost") else {
+            XCTFail("manager should NOT be nil")
+            return
+        }
+
+        let expectation = self.expectation(description: "listener closure should be executed")
+        var networkReachabilityStatus: NetworkReachabilityStatus?
+
+        // When
+        manager.startListening { status in
+            guard networkReachabilityStatus == nil else { return }
+            networkReachabilityStatus = status
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 1, handler: nil)
+
+        // Then
+        XCTAssertEqual(networkReachabilityStatus, .reachable(.ethernetOrWiFi))
+    }
+
+    func test_DefaultInitializer_StartListening_ClosureIsCalled_WhenStartListeningIsCalled() {
+        // Given
+        let manager = NetworkReachabilityManager()
+        let expectation = self.expectation(description: "listener closure should be executed")
+
+        var networkReachabilityStatus: NetworkReachabilityStatus?
+
+        // When
+        manager?.startListening { status in
+            networkReachabilityStatus = status
+            expectation.fulfill()
+        }
+        waitForExpectations(timeout: 1, handler: nil)
+
+        // Then
+        XCTAssertEqual(networkReachabilityStatus, .reachable(.ethernetOrWiFi))
+    }
+}
