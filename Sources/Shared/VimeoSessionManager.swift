5f4e943ee9ab0eec22386c84602df2894928f083
Rogerio de Paula Assis <rogerio@vimeo.com>
2019-09-17 16:39:33 -0400

    Adds getter and setter for acceptableContentTypes

--- Sources/Shared/VimeoSessionManager.swift
+++ Sources/Shared/VimeoSessionManager.swift
@@ -34,16 +34,24 @@ private typealias SessionManagingDataTaskProgress = (Progress) -> Void
 /** `VimeoSessionManager` handles networking and serialization for raw HTTP requests.  It is a direct subclass of `AFHTTPSessionManager` and it's designed to be used internally by `VimeoClient`.  For the majority of purposes, it would be better to use `VimeoClient` and a `Request` object to better encapsulate this logic, since the latter provides richer functionality overall.
  */
 final public class VimeoSessionManager: AFHTTPSessionManager, SessionManaging {
-    
-    // MARK: Initialization
-    
+
+    // MARK: - Public
+
+    /// Getter and setter for acceptableContentTypes propert on response serializer
+    @objc public var acceptableContentTypes: Set<String>? {
+        get { return responseSerializer.acceptableContentTypes }
+        set { responseSerializer.acceptableContentTypes = newValue }
+    }
+
+    // MARK: - Initialization
+
     /**
      Creates a new session manager
-     
+
      - parameter baseUrl: The base URL for the HTTP client
      - parameter sessionConfiguration: Object describing the URL session policies for this session manager
      - parameter requestSerializer:    Serializer to use for all requests handled by this session manager
-     
+
      - returns: an initialized `VimeoSessionManager`
      */
     required public init(
@@ -52,36 +60,36 @@ final public class VimeoSessionManager: AFHTTPSessionManager, SessionManaging {
         requestSerializer: VimeoRequestSerializer
     ) {
         super.init(baseURL: baseUrl, sessionConfiguration: sessionConfiguration)
-        
+
         self.requestSerializer = requestSerializer
         self.responseSerializer = VimeoResponseSerializer()
     }
-    
+
     required public init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     public func invalidate() {
         self.invalidateSessionCancelingTasks(false)
     }
-    
+
     public func request(
         with endpoint: EndpointType,
         then callback: @escaping (SessionManagingResponse<Any>) -> Void
     ) -> Cancelable? {
         let path = endpoint.uri
         let parameters = endpoint.parameters
-        
+
         let success: SessionManagingDataTaskSuccess = { dataTask, value in
             let response = SessionManagingResponse(task: dataTask, value: value, error: nil)
             callback(response)
         }
-        
+
         let failure: SessionManagingDataTaskFailure = { dataTask, error in
             let response = SessionManagingResponse<Any>(task: dataTask, value: nil, error: error)
             callback(response)
         }
-        
+
         switch endpoint.method {
         case .get:
             return self.get(path, parameters: parameters, progress: nil, success: success, failure: failure)
@@ -95,16 +103,16 @@ final public class VimeoSessionManager: AFHTTPSessionManager, SessionManaging {
             return self.delete(path, parameters: parameters, success: success, failure: failure)
         case .connect, .head, .options, .trace:
             return nil
-        }                
+        }
     }
 }
 
 extension VimeoSessionManager: AuthenticationListeningDelegate {
     // MARK: - Authentication
-    
+
     /**
      Called when authentication completes successfully to update the session manager with the new access token
-     
+
      - parameter account: the new account
      */
     public func clientDidAuthenticate(with account: VIMAccount) {
@@ -112,13 +120,13 @@ extension VimeoSessionManager: AuthenticationListeningDelegate {
         else {
             return
         }
-        
+
         let accessToken = account.accessToken
         requestSerializer.accessTokenProvider = {
             return accessToken
         }
     }
-    
+
     /**
      Called when a client is logged out and the current account should be cleared from the session manager
      */
@@ -127,7 +135,7 @@ extension VimeoSessionManager: AuthenticationListeningDelegate {
             else {
             return
         }
-        
+
         requestSerializer.accessTokenProvider = nil
-    }    
+    }
 }
