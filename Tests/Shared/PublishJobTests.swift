ebb444a8368a9b2f1f96bb5f6e9fe269383f5fb4
Jason Hawkins <hellojason@me.com>
2020-01-09 17:17:04 -0500

    Add tests for parsing publish job destinations

--- Tests/Shared/PublishJobTests.swift
+++ Tests/Shared/PublishJobTests.swift
@@ -44,11 +44,10 @@ class PublishJobTests: XCTestCase {
         let json: [String: Any] = [
             "first_publish_date": "2015-12-17T21:32:44+00:00",
             "destinations": "",
-            "resource_key": "3ebd4a2becabbee30ca2baae3a23cebca1137063"
+            "resource_key": "03c2e13be74abec3a3aeba223ae6cd01b3caebba"
         ]
 
         let publishJob = try VIMObjectMapper.mapObject(responseDictionary: json) as PublishJob
-
         var calendar = Calendar.current
         TimeZone(secondsFromGMT: 0).map { calendar.timeZone = $0 }
         let year = try calendar.component(.year, from: XCTUnwrap(publishJob.firstPublishDate))
@@ -64,5 +63,31 @@ class PublishJobTests: XCTestCase {
         XCTAssertEqual(hour, 21)
         XCTAssertEqual(minute, 32)
         XCTAssertEqual(second, 44)
+    }
+
+    func test_publishJob_parsesPublishDestinations_asExpected() throws {
+        let youtube: [String: Any] = [
+            "status": "finished",
+            "third_party_post_url": "https://www.youtube.com/watch?v=i72F_nPdLoz",
+            "third_party_post_id": "i72F_nPdLoz"
+        ]
+
+        let destinations: [String: Any] = [
+            "youtube": youtube as Any
+        ]
+
+        let job: [String: Any] = [
+            "first_publish_date": "2015-12-17T21:32:44+00:00",
+            "destinations": destinations as Any,
+            "resource_key": "03c2e13be74abec3a3aeba223ae6cd01b3caebba"
+        ]
+
+        let publishJob = try VIMObjectMapper.mapObject(responseDictionary: job) as PublishJob
+        XCTAssertNotNil(publishJob.destinations)
+        XCTAssertNotNil(publishJob.destinations?.youtube)
+        XCTAssertEqual(publishJob.destinations?.youtube?.status, .finished)
+        XCTAssertEqual(publishJob.destinations?.youtube?.status?.description, "finished")
+        XCTAssertEqual(publishJob.destinations?.youtube?.thirdPartyPostURL, "https://www.youtube.com/watch?v=i72F_nPdLoz")
+        XCTAssertEqual(publishJob.destinations?.youtube?.thirdPartyPostID, "i72F_nPdLoz")
     }
 }
