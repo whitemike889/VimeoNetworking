35e62dd42853cae49759f8d9914cfffa61f790d6
Jason Hawkins <hellojason@me.com>
2020-01-13 17:01:16 -0500

    Replace local test data with fixture from server response

--- Tests/Shared/PublishJobTests.swift
+++ Tests/Shared/PublishJobTests.swift
@@ -27,26 +27,26 @@ import XCTest
 @testable import VimeoNetworking
 
 class PublishJobTests: XCTestCase {
-    func test_publishJob_parsesAsExpected() throws {
-        let json: [String: Any] = [
-            "first_publish_date": "2015-12-17T21:32:44+00:00",
-            "destinations": "",
-            "resource_key": "3ebd4a2becabbee30ca2baae3a23cebca1137063"
-        ]
 
+    private var json: VimeoClient.ResponseDictionary {
+        guard let json = ResponseUtilities.loadResponse(
+            from: "publish-to-social-response.json"
+        ) else {
+            XCTFail()
+            return [:]
+        }
+
+        return json
+    }
+
+    func test_publishJob_parsesAsExpected() throws {
         let publishJob = try VIMObjectMapper.mapObject(responseDictionary: json) as PublishJob
         XCTAssertNotNil(publishJob.firstPublishDate)
         XCTAssertNotNil(publishJob.resourceKey)
-        XCTAssertEqual(publishJob.resourceKey, "3ebd4a2becabbee30ca2baae3a23cebca1137063")
+        XCTAssertEqual(publishJob.resourceKey, "43ccd2e018c0fc54feedf7fd19e018c0fc2897de")
     }
 
     func test_firstPublishDate_isFormattedCorrectly() throws {
-        let json: [String: Any] = [
-            "first_publish_date": "2015-12-17T21:32:44+00:00",
-            "destinations": "",
-            "resource_key": "03c2e13be74abec3a3aeba223ae6cd01b3caebba"
-        ]
-
         let publishJob = try VIMObjectMapper.mapObject(responseDictionary: json) as PublishJob
         var calendar = Calendar.current
         TimeZone(secondsFromGMT: 0).map { calendar.timeZone = $0 }
@@ -65,32 +65,16 @@ class PublishJobTests: XCTestCase {
         XCTAssertEqual(second, 44)
     }
 
-    func test_publishJob_parsesPublishDestinations_asExpected() throws {
-        let youtube: [String: Any] = [
-            "status": "finished",
-            "third_party_post_url": "https://www.youtube.com/watch?v=i72F_nPdLoz",
-            "third_party_post_id": "i72F_nPdLoz"
-        ]
-
-        let destinations: [String: Any] = [
-            "youtube": youtube as Any
-        ]
-
-        let job: [String: Any] = [
-            "first_publish_date": "2015-12-17T21:32:44+00:00",
-            "destinations": destinations as Any,
-            "resource_key": "03c2e13be74abec3a3aeba223ae6cd01b3caebba"
-        ]
-
-        let publishJob = try VIMObjectMapper.mapObject(responseDictionary: job) as PublishJob
+    func test_publishJob_parsesPublishDestination_asExpected() throws {
+        let publishJob = try VIMObjectMapper.mapObject(responseDictionary: json) as PublishJob
         XCTAssertNotNil(publishJob.destinations)
-        XCTAssertNotNil(publishJob.destinations?.youtube)
-        XCTAssertEqual(publishJob.destinations?.youtube?.status, .finished)
-        XCTAssertEqual(publishJob.destinations?.youtube?.status?.description, "finished")
+        XCTAssertNotNil(publishJob.destinations?.facebook)
+        XCTAssertEqual(publishJob.destinations?.facebook?.status, .finished)
+        XCTAssertEqual(publishJob.destinations?.facebook?.status?.description, "finished")
         XCTAssertEqual(
-            publishJob.destinations?.youtube?.thirdPartyPostURL,
-            "https://www.youtube.com/watch?v=i72F_nPdLoz"
+            publishJob.destinations?.facebook?.thirdPartyPostURL,
+            "https://facebook.com/150285126584258/videos/2452706093602193/"
         )
-        XCTAssertEqual(publishJob.destinations?.youtube?.thirdPartyPostID, "i72F_nPdLoz")
+        XCTAssertEqual(publishJob.destinations?.facebook?.thirdPartyPostID, "2370929360645821")
     }
 }
